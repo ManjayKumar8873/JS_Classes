0cd9006949c964faecb0bc3926b6d6303afcaf99
Manjay Kumar <mkp88736@gmail.com>
2025-04-01 19:53:57 +0000

    datatypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert(3+3) //we are using nodejs, not browser
+
+ console.log(3
+    +
+    3) //code readability should be high
+
+console.log("manjay");
+
+let name = "Manjay"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalon value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "Manjay");
+console.log(typeof undefined); // undefined 
+console.log(typeof nul); // object
+
+
+
+
