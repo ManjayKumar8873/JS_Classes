bf562e1fbd8fcf69cb2801b62231ca08b01e110b
Manjay Kumar <mkp88736@gmail.com>
2025-04-02 05:18:23 +0000

    operation discussed

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -37,3 +37,45 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+// ********************** Operations ****************************
+
+
+let value = 3
+
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Manjay"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+let gamecounter = 100
+gamecounter++;
+
+console.log(gamecounter);
+
+// link to study
+// https://tc39.es/ecma262/#sec-type-conversion
+
+
+
+
+
+
+
