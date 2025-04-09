ecc4102e2a9b219c0c38f3414451ae59d079d0fd
Manjay Kumar <mkp88736@gmail.com>
2025-04-09 05:01:51 +0000

    05 objects

---
+++ 02_basic/05_objects.js
@@ -0,0 +1,14 @@
+
+const course = {
+    coursename: "JS in Hindi",
+    price: "999",
+    courseInstructor: "Manjay"
+}
+//console.log(course.courseInstructor);
+
+//+++++++ OR +++++++++
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
