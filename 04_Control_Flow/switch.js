d038c7fefe099016e34cab721886f7bdd0d9822f
Manjay Kumar <mkp88736@gmail.com>
2025-04-17 14:11:05 +0000

    Switch case

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,63 @@
+// switch(key){
+//     case value:
+
+//         break;
+
+//     default:
+    
+//         break;
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("Feb");
+        break;
+
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
+//  To String Values 
+
+const months = "March"
+
+switch (months) {
+    case "Jan":
+        console.log("January");
+        break;
+
+    case "Fab":
+        console.log("Feb");
+        break;
+
+    case "March":
+        console.log("March");
+        break;
+
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
+
